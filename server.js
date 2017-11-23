64ed8a81a9028c04f3a9544e26cf9d8853390c32
Lav39 <lavkushkumar7654@gmail.com>
2017-11-23 20:14:01 +0530

    change server port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 let app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.use(express.static('src'));
 app.listen(PORT, function() {
     console.log('Node app is running on port', PORT);
